e3477846f4bfeb4e21807f578b1533b0b3ece233
Kalley Wilkerson <kalleywra@gmail.com>
2020-06-29 13:23:43 -0300

    Project created

---
+++ src/test/java/com/kalley/javacourse/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kalley.javacourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
