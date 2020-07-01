edb7b7791dfdf5ab4de0d01c0fefcb68c1cace04
Kalley Wilkerson <kalleywra@gmail.com>
2020-07-01 17:07:57 -0300

    One-to-one association with Order'

--- src/main/java/com/kalley/javacourse/config/TestConfig.java
+++ src/main/java/com/kalley/javacourse/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.kalley.javacourse.entities.Category;
 import com.kalley.javacourse.entities.Order;
 import com.kalley.javacourse.entities.OrderItem;
+import com.kalley.javacourse.entities.Payment;
 import com.kalley.javacourse.entities.Product;
 import com.kalley.javacourse.entities.User;
 import com.kalley.javacourse.entities.enums.OrderStatus;
@@ -79,6 +80,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 
 }
