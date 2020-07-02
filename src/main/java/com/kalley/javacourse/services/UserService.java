daf80cf7c3246f6a703807443114106c846e0247
Kalley Wilkerson <kalleywra@gmail.com>
2020-07-01 22:13:15 -0300

    Inserting user

--- src/main/java/com/kalley/javacourse/services/UserService.java
+++ src/main/java/com/kalley/javacourse/services/UserService.java
@@ -21,5 +21,9 @@ public class UserService {
 	public User findById(Long id) {
 		return repository.findById(id).get();
 	}
+	
+	public User insert(User obj) {
+		return repository.save(obj);
+	}
 
 }
