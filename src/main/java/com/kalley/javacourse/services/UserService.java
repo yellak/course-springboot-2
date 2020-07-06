f211964c02d110df99fe2eee1cb463ef3b60869a
Kalley Wilkerson <kalleywra@gmail.com>
2020-07-06 14:27:51 -0300

    Updating user

--- src/main/java/com/kalley/javacourse/services/UserService.java
+++ src/main/java/com/kalley/javacourse/services/UserService.java
@@ -29,5 +29,17 @@ public class UserService {
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
+	
+	public User update(Long id, User obj) {
+		User entity = repository.getOne(id);
+		udpateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void udpateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+	}
 
 }
